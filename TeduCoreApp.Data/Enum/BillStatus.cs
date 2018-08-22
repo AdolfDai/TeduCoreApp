a3444b83e77e65171cd1e63d8552384e1910daf8
AdolfDai <adolfdai88@gmail.com>
2018-08-22 10:57:26 +0700

    Create App

---
+++ TeduCoreApp.Data/Enum/BillStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduCoreApp.Data.Enum
+{
+    public enum BillStatus
+    {
+        New,
+        InProgress,
+        Returned,
+        Cancelled,
+        Completed
+    }
+}
---
+++ TeduCoreApp.Infrastructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduCoreApp.Infrastructure.SharedKernel
+{
+    //kiểu Generic T : ứng với từng loại data entity sẽ có kiểu Id khác nhau
+    public class DomainEntity<T>
+    {
+        public T Id { get; set; }
+        //hàm istransient sẽ kiểm tra có giá trị gán vào kiểu T chưa
+        //trả về True : khi đã có giá trị gán
+        //trả về False: khi chưa có giá trị gán
+        public bool IsTransient()
+        {
+            return Id.Equals(default(T));
+        }
+    }
+    
+}
